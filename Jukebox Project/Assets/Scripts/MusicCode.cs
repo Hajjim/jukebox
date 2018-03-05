7a0b71efc470bac55ad8509e52ee5142267872cb
Hajji M <hajjimohamed12@hotmail.com>
2018-03-05 17:49:38 +0100

    [ADD] JukeBox V2.0 (ProblÃme de mÃmoire rÃsolu -> Desallocation aussi)

--- Jukebox Project/Assets/Scripts/MusicCode.cs
+++ Jukebox Project/Assets/Scripts/MusicCode.cs
@@ -6,7 +6,8 @@ using UnityEngine.Audio;
 using System.IO;
 
 [RequireComponent(typeof(AudioSource))]
-public class MusicCode : MonoBehaviour {
+public class MusicCode : MonoBehaviour
+{
 
     public GameObject itemPrefab; //ma liste d'élements clips
 
@@ -14,7 +15,6 @@ public class MusicCode : MonoBehaviour {
 
     //public AudioClip[] clips; //les musiques contenant les info 
     public List<AudioClip> clips = new List<AudioClip>(); //les musiques contenant les info 
-    private int actuelMusic;
     public Slider TimerGraphique;
 
     public Text clipTitre;
@@ -25,50 +25,57 @@ public class MusicCode : MonoBehaviour {
     private int playTimer;
     private int secondes;
     private int minutes;
+    private int numbermusic=0;
 
     // ajout pour prendre dans repertoire
     private FileInfo[] files;
-    string path = "./"; // chemin RELATIF d'où l'application COMPILEE tourne. Pas le mode editeur sur unity. (donc quand j'aurai l'apk, ça se trouvera sur l'endroit même)
+    // string path = "./"; // chemin RELATIF d'où l'application COMPILEE tourne. Pas le mode editeur sur unity. (donc quand j'aurai l'apk, ça se trouvera sur l'endroit même)
     // -----
-    
+    static string path = "C:/Users/hajji/OneDrive/Projects/"; // C:\Users\hajji\Documents\
+    string[] fileList = Directory.GetFiles(path, "*.wav", SearchOption.TopDirectoryOnly);
+    AudioClip musicAJouer;
+
     void Start()
     {
         source = GetComponent<AudioSource>(); //création d'un composant Audiosource
-        actuelMusic = 0; 
         //Play(); //pour démarrer directement
         TimerGraphique.enabled = false; //j'empêche de toucher le slider
         GameObject container = GameObject.Find("Elements");
         // --------------------------
-        if (Application.isEditor)
-        {
-            path = "C:/Users/hajji/OneDrive/Projects";
-        }
-        DirectoryInfo info = new DirectoryInfo(path);
-        files = info.GetFiles();
 
-        foreach (FileInfo f in files)
+        /* if (Application.isEditor)
+         {
+             path = "C:/Users/hajji/OneDrive/Projects/"; // C:\Users\hajji\Documents\
+         }*/
+        /* DirectoryInfo info = new DirectoryInfo(path);
+         files = info.GetFiles();*/
+
+        //Charge liste string
+
+        foreach (string chemin in fileList)
         {
-            if (f.FullName.IndexOf("wav") > -1)
-            {
-                WWW www = new WWW("file://" + f.FullName);
-                AudioClip myAudioClip = www.GetAudioClip();
-                while (myAudioClip.loadState != AudioDataLoadState.Loaded) { } //ne fait rien tant que c'est pas load 
-                AudioClip clip = www.GetAudioClip(false); //useless pour le 3D donc false
-                //string[] parts = path.Split('\\'); 
-                clip.name = Path.GetFileNameWithoutExtension(f.Name); //sans l'extension .wav
-                clips.Add(clip);
-            }
-        }
-        // --------------------------
-        foreach (AudioClip clip in clips)
-        {
-            Debug.Log(clip.name);
+            Debug.Log(chemin.Replace(path, "").Replace(".wav", ""));
             GameObject item = Instantiate(itemPrefab) as GameObject;
-            item.name = clip.name;
-            item.GetComponentInChildren<Text>().text = clip.name;
+            item.name = Path.GetFileNameWithoutExtension(chemin);
+            item.GetComponentInChildren<Text>().text = Path.GetFileNameWithoutExtension(chemin);
             item.transform.parent = container.transform;
         }
-        Play();
+            playMusic(numbermusic);
+
+        // --------------------------
+
+
+    }
+
+    private void playMusic(int i)
+    {
+        WWW url = new WWW("file://" + fileList[i]);
+        musicAJouer = url.GetAudioClip(false);//useless pour le 3D donc false
+        while (musicAJouer.loadState != AudioDataLoadState.Loaded) { } //ne fait rien tant que c'est pas load 
+        //musicAJouer = url.GetAudioClip(false); 
+        //string[] parts = path.Split('\\'); 
+        musicAJouer.name = Path.GetFileNameWithoutExtension(fileList[i]); //sans l'extension .wav
+        Play(musicAJouer);       
     }
 
     //Partie Coroutine :
@@ -78,80 +85,87 @@ public class MusicCode : MonoBehaviour {
 
     IEnumerator WaitMusicEnd()  //Une co-routine renvoie toujours un type spécial : IEnumarator
     {
+       
         while (source.isPlaying)
         {
             TimerGraphique.value += Time.deltaTime; //utilisation du deltatime pour incrémenter dans le slider
-            if (TimerGraphique.value >= source.clip.length)
+            if (TimerGraphique.value >= musicAJouer.length) // && numbermusic < fileList.Length
             {
-                Suivant();
-
+                numbermusic++;
+                numbermusic = numbermusic % fileList.Length; // 1 2 3 ... 1 2 3 ... 1 2 3 ... (opti)         
+                Suivant(musicAJouer, numbermusic);
             }
             playTimer = (int)source.time; //temps depuis le début pour cette même musique
-            ShowPlayTime();
+            //ShowPlayTime();
             yield return null; //C’est au niveau de l’instruction yield que la pause s’effectue.
         }
-        Suivant();
+        numbermusic++;
+        numbermusic = numbermusic % fileList.Length; // 1 2 3 ... 1 2 3 ... 1 2 3 ... (opti)    
+        Suivant(musicAJouer, numbermusic);
     }
 
 
-    public void Play()
+    public void Play(AudioClip music)
     {
-            //la musique jouer sera la derniere ajouté?!
-            //actuelMusic--;
-            if (actuelMusic < 0) 
-            {   //boucle la music
-                actuelMusic = clips.Count - 1; //au lieu de Length, j'utilise count car liste 
-            }
-            source.clip = clips[actuelMusic];
-            //slider taille total en fonction de l'audiosource
-            TimerGraphique.maxValue = source.clip.length;
-            //slider remis au début
-            if (TimerGraphique.value == 0)
-            {
-                TimerGraphique.value = 0;
-            }
-            //Play sur l'audio source (appartient à unity et donc différent de ma méthode Play())
-            source.Play();
-            //j'affiche le titre
-            ShowTitreClip();
-            //ma coroutine
-            StartCoroutine("WaitMusicEnd");
-        
+        //la musique jouer sera la derniere ajouté?!
+        //actuelMusic--;
+        /*if (actuelMusic < 0)
+        {   //boucle la music
+            actuelMusic = clips.Count - 1; //au lieu de Length, j'utilise count car liste 
+        }*/
+        source.clip = music;
+        //slider taille total en fonction de l'audiosource
+        //TimerGraphique.maxValue = source.clip.length
+        TimerGraphique.maxValue = music.length;
+        //slider remis au début
+        TimerGraphique.value = 0;
+        //Play sur l'audio source (appartient à unity et donc différent de ma méthode Play())
+        source.Play();
+        //j'affiche le titre
+        ShowTitreClip(numbermusic);
+        ShowPlayTime(music);
+        //ma coroutine
+        StartCoroutine("WaitMusicEnd");
+
     }
 
-    public void Suivant()
+    public void Suivant(AudioClip music, int k)
     {
         source.Stop(); //j'arrête la source
         StopCoroutine("WaitMusicEnd"); //je met un stop sur la coroutine
-        actuelMusic++; //incrémentation pour savoir où on se situe dans la liste
+       /* actuelMusic++; //incrémentation pour savoir où on se situe dans la liste
         //dans le cas où j'arrive à la fin de la liste
         if (actuelMusic > clips.Count - 1)
         {
             actuelMusic = 0;
-        }
-        source.clip = clips[actuelMusic];
+        }*/
+        music.UnloadAudioData(); //désallouer
+        playMusic(k);
+
+       /* source.clip = music;
         //Play sur l'audio source (appartient à unity et donc différent de ma méthode Play())
-        source.Play();
+        source.Play(); // PLAY de UNITY
         //j'affiche le titre
-        ShowTitreClip();
+        ShowTitreClip(k);
+        ShowPlayTime(music);
         //je remet le slider à nul en définissant bien le max du slider
-        TimerGraphique.maxValue = source.clip.length;
+        TimerGraphique.maxValue = music.length;
         TimerGraphique.value = 0;
         //ma coroutine
-        StartCoroutine("WaitMusicEnd");
+        StartCoroutine("WaitMusicEnd");*/
     }
 
 
     //méthode pour afficher le titre du clip
-    void ShowTitreClip()
+    void ShowTitreClip(int k)
     {
-        clipTitre.text = source.clip.name;
-        MusicLongueur = (int)source.clip.length; //longueur de la musique attribué à MusicLongueur
+        clipTitre.text = Path.GetFileNameWithoutExtension(fileList[k]);
     }
 
     //méthode pour afficher le temps en minute seconde
-    void ShowPlayTime()
+    void ShowPlayTime(AudioClip music)
     {
+        MusicLongueur = (int)music.length;
         secondes = playTimer % 60;
         minutes = (playTimer / 60) % 60;
         clipTotalTime.text = ((MusicLongueur / 60) % 60) + ":" + (MusicLongueur % 60).ToString("D2"); //affichage deux decimal
